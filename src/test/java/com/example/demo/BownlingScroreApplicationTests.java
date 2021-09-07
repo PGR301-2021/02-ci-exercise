5b2d4be7777dff526e273b1adc97fdcc37b385e8
Kaikka <kaileeto@gmail.com>
2021-09-07 11:29:41 +0200

    Test to check for default return 0 score

--- src/test/java/com/example/demo/BownlingScroreApplicationTests.java
+++ src/test/java/com/example/demo/BownlingScroreApplicationTests.java
@@ -3,11 +3,22 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class BownlingScroreApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void createGameAndGet0Score() {
+
+		Game game = new Game();
+		int score = game.score();
+
+		assertEquals(score, 0);
 	}
 
 }
