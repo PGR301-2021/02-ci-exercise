c879f132abcd8dc5b0610c22b5bd6be62e543433
abhil <abhilash.kamboj@gmail.com>
2021-09-07 11:25:18 +0200

    first test

--- src/test/java/com/example/demo/BownlingScroreApplicationTests.java
+++ src/test/java/com/example/demo/BownlingScroreApplicationTests.java
@@ -3,6 +3,9 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 @SpringBootTest
 class BownlingScroreApplicationTests {
 
@@ -10,4 +13,10 @@ class BownlingScroreApplicationTests {
 	void contextLoads() {
 	}
 
+	@Test
+	void testsimplecase(){
+		Game g = new Game();
+		g.roll(9);
+		assertEquals(9, 9);
+	}
 }
